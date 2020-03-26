c1de98ac74322fae25d2b8605246e771afd5c743
edasilvamendes <edi.da.silva.mendes@disney.com>
2020-03-26 12:01:06 +0100

    Modif Favoris

--- src/screens/games.js
+++ src/screens/games.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, View } from 'react'
+import React, { useState, useEffect } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-//import uuid from 'uuid/v4'
+import uuid from 'uuid/v4'
 import allTheActions from '../actions'
 
 /* eslint-disable react/jsx-key */
@@ -10,20 +10,21 @@ import data from '../data.json'
 const Jeux = data.Jeux
 
 const Game = props => {
-  //console.log('GAME', props)
+  console.log('GAME', props)
   const [game, setGame] = useState('')
 
-  console.log(props)
+  /*console.log(props)
   console.log(game)
   console.log(setGame)
-
-  console.log(Jeux)
+  console.log(Jeux)*/
 
   useEffect(() => {
     //console.log('OKOK' + game)
   })
 
-  /*const onSubmit = e => {
+  const onSubmit = e => {
+    //console.log('HAAHAHHA')
+
     e.preventDefault()
     const { actions } = props
     console.log('TEST:' + actions.games)
@@ -32,20 +33,24 @@ const Game = props => {
       id: uuid()
     })
     setGame('')
-  }*/
+    //console.log('ok')
+  }
 
   return (
-    <View /*onSubmit={onSubmit}*/>
+    <div>
       <div>
         {Jeux.map(s => (
-          <div>
+          <div key={s.id}>
             <p key={s.id}>ID: {s.id}</p>
             <p key={s.id}>TITLE: {s.title}</p>
             <p key={s.id}>DESCIRPTION{s.description}</p>
+            <form onSubmit={onSubmit}>
+              <button>AJOUT FAVORIS</button>
+            </form>
           </div>
         ))}
       </div>
-    </View>
+    </div>
   )
 }
 
